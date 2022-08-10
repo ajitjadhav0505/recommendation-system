dd5ec52146da0a4d857416eed69c4674a589b1f3
Ajit Jadhav <103515761+ajitjadhav0505@users.noreply.github.com>
2022-08-10 23:16:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from keras.layers.convolutional import MaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+import numpy as np
+import os
+import pickle
+from tqdm import tqdm
+from numpy.linalg import norm
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    MaxPooling2D()
+])
+
+#print(model.summary())
+
+def extract_features(img_path,model):
+    img = image.load_img(img_path,target_size=(224,224))
+    img_array = image.img_to_array(img)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalized_result = result / norm(result)
+
+    return normalized_result
+
+filenames = []
+
+for file in os.listdir('images'):
+    filenames.append(os.path.join('images',file))
+
+feature_list = []
+
+for file in tqdm(filenames):
+    feature_list.append(extract_features(file,model))
+
+
+pickle.dump(feature_list,open('features.pkl','wb'))
+pickle.dump(filenames,open('filenames.pkl','wb'))
